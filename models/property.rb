c12822a05980b9d53f383e897b4629fbb53a0022
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-04-11 19:00:16 +0700

    save model to database

--- models/property.rb
+++ models/property.rb
@@ -5,7 +5,7 @@ class Property
   include ActiveModel::Model
   TABLE_NAME = 'properties'
 
-  attr_accessor :id,
+  attr_accessor :rowid,
                 :title,
                 :property_type,
                 :address,
@@ -14,16 +14,53 @@ class Property
                 :email,
                 :phone
 
-  def initialize
+  def initialize(property_id = 0)
     @db = Database.instance.connection
+    if property_id == 0
+      create
+    else
+      self.rowid = property_id
+      row = @db.execute("SELECT * FROM #{TABLE_NAME} WHERE rowid = ?", property_id)
+      raise ArgumentError, 'no such property id' unless row.count > 0
+      values = row.first
+      # bit rude methot to digg into db results...
+      self.title = values[0]
+      self.property_type = values[1]
+      self.address = values[2]
+      self.rate = values[3]
+      self.max_guests = values[4]
+      self.email = values[5]
+      self.phone = values[6]
+    end
   end
 
   def human_readable_id
-    "ABC#{id}DEF#{id+1}"
+    "ABC#{rowid}DEF#{rowid+1}"
   end
 
   # creates new record
   def create
     @db.execute("INSERT INTO #{TABLE_NAME} VALUES('', 0, '', 0, 0, '', '')")
+    self.rowid = @db.last_insert_row_id
+  end
+
+  # requires hash of params
+  # title, property_type, address, rate, max_guests, email, phone
+  # any or all could be nil
+  def update(params = {})
+    self.title = params[:title] || ''
+    self.property_type = params[:property_type] || 0
+    self.address = params[:address] || ''
+    self.rate = params[:rate] || 0
+    self.max_guests = params[:max_guests] || 0
+    self.email = params[:email] || ''
+    self.phone = params[:phone] || ''
+  end
+
+  def save
+    @db.execute("UPDATE #{TABLE_NAME} SET title = ?, property_type = ?,
+      address = ?, rate = ?, max_guests = ?, email = ?, phone = ?",
+      self.title, self.property_type, self.address, self.rate,
+      self.max_guests, self.email, self.phone)
   end
 end
--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -3,31 +3,66 @@ require "property"
 require "database"
 
 describe "Property" do
+  let(:db) { Database.instance }
   let(:property) { Property.new }
+
+  before do
+    db.setup
+  end
 
   it "creates blank property object" do
     expect(property).to be_instance_of(Property)
   end
 
+  describe "property id" do
+    it "creates new property, if no id specified" do
+      expect(property.rowid).to_not be nil
+    end
+
+    it "returns existing property" do
+      property2 = Property.new(property.rowid)
+      expect(property2.rowid).to eq property.rowid
+    end
+
+    it "raises ArgumentError if no such property found" do
+      expect{Property.new(-1)}.to raise_error(ArgumentError)
+    end
+  end
+
   describe ".human_readable_id" do
     it "returns human readable id" do
-      property.id = 1
+      property.rowid = 1
       expect(property.human_readable_id).to eq 'ABC1DEF2'
     end
   end
 
-  describe "create" do
-    let(:db) { Database.instance }
-
-    before do
-      db.setup
-    end
-
+  describe ".create" do
     it "creates new database record" do
       rows_before = db.connection.execute("SELECT * FROM properties")
       property.create
       rows_after = db.connection.execute("SELECT * FROM properties")
-      expect(rows_after.count - rows_before.count).to eq 1
+      expect(rows_after.count > rows_before.count).to be true
+    end
+
+    it "returns rowid" do
+      expect(property.create).to_not be nil
+    end
+  end
+
+  describe ".update" do
+    it "sets object attributes" do
+      property.update(title: 'My house')
+      expect(property.title).to eq 'My house'
+    end
+  end
+
+  describe ".save" do
+    it "saves changes to database" do
+      property.update(title: 'My house')
+      property.save
+      row = db.connection.execute("SELECT * FROM properties WHERE rowid = ?",
+        property.rowid)
+      expect(row[0][0]).to eq 'My house'
     end
   end
 end
